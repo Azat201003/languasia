86feb206388a571c0cf7f72b1670a594942eead6
Azat201003 <zanelane87@gmail.com>
2026-01-26 23:26:11 +0300

    add endpoints and web socket base

--- backend/src/routes.go
+++ backend/src/routes.go
@@ -20,8 +20,14 @@ type creditionals struct {
 func register(c *echo.Context) error {
 	request := new(creditionals)
 	if err := c.Bind(request); err != nil {
+		c.String(http.StatusBadRequest, fmt.Sprintf("Bad data: %v", err))
 		return err
 	}
+
+	if len(request.Password) < 2 {
+		return c.String(http.StatusBadRequest, "Bad password: too short")
+	}
+
 	passwordHash, err := bcrypt.GenerateFromPassword([]byte(request.Password), bcrypt.DefaultCost)
 	if err != nil {
 		c.String(http.StatusUnprocessableEntity, fmt.Sprintf("Cannot get hash of password: %v", err.Error()))
@@ -165,3 +171,143 @@ func connectWebSocket(c *echo.Context) error {
 	return wsh.ConnectWebSocket(c.Response(), c.Request())
 }
 
+func deleteUser(c *echo.Context) error {
+	userIdString := c.ParamOr("user_id", "0")
+
+	userId, err := strconv.ParseUint(userIdString, 10, 64)
+	if err != nil {
+		c.String(http.StatusBadRequest, fmt.Sprintf("Cannot parse user_id: %v", err.Error()))
+		return err
+	}
+
+	if c.Get("user_id") != userId {
+		return c.String(http.StatusUnauthorized, "token user_id and put user_id not match")
+	}
+
+	err = dbc.DeleteUser(userId)
+	if err != nil {
+		c.String(http.StatusTeapot, fmt.Sprintf("Cannot delete user: %v", err.Error()))
+		return err
+	}
+
+	return c.String(http.StatusOK, "User succefully deleted")
+}
+
+type updateUserRequest struct {
+	Description string `json:"description"`
+	Password string `json:"password"`
+}
+
+func updateUser(c *echo.Context) error {	
+	userIdString := c.ParamOr("user_id", "0")
+
+	userId, err := strconv.ParseUint(userIdString, 10, 64)
+	if err != nil {
+		c.String(http.StatusBadRequest, fmt.Sprintf("Cannot parse user_id: %v", err.Error()))
+		return err
+	}
+
+	if c.Get("user_id") != userId {
+		return c.String(http.StatusUnauthorized, fmt.Sprintf("token user_id and put user_id not match: %v != %v", userId, c.Get("user_id")))
+	}
+	
+	request := new (updateUserRequest)
+	if err := c.Bind(request); err != nil {
+		c.String(http.StatusBadRequest, fmt.Sprintf("Cannot parse user data: %v", err.Error()))
+		return err
+	}
+	
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(request.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.String(http.StatusUnprocessableEntity, fmt.Sprintf("Cannot get hash of password: %v", err.Error()))
+		return err
+	}
+
+	err = dbc.UpdadateUser(&User{
+		UserId: userId,
+		PasswordHash: passwordHash,
+		Description: request.Description,
+	})
+	if err != nil {
+		return err
+	}
+
+	return c.String(http.StatusOK, "Succesfully updated")
+}
+
+func addLanguage(c *echo.Context) error {
+	request := new (Language)
+	if err := c.Bind(request); err != nil {	
+		c.String(http.StatusBadRequest, fmt.Sprintf("Cannot parse language data: %v", err.Error()))
+		return err
+	}
+
+	var err error
+	request.UserId, err = strconv.ParseUint(c.ParamOr("user_id", "0"), 10, 64)
+	if err != nil {
+		return err
+	}
+
+	if c.Get("user_id") != request.UserId {
+		return c.String(http.StatusUnauthorized, "token user_id and put user_id not match")
+	}
+	
+	return dbc.AddLanguage(request)
+} 
+
+func deleteLanguage(c *echo.Context) error {
+	userId, err := strconv.ParseUint(c.ParamOr("user_id", "0"), 10, 64)
+	if err != nil {
+		return err
+	}
+	
+	if c.Get("user_id") != userId {
+		return c.String(http.StatusUnauthorized, "token user_id and put user_id not match")
+	}
+
+	languageId, err := strconv.ParseUint(c.ParamOr("language_id", "0"), 10, 64)
+	if err != nil {
+		return err
+	}
+
+	return dbc.DeleteLanguage(userId, languageId)
+}
+
+
+func addHobby(c *echo.Context) error {
+	request := new (Hobby)
+	if err := c.Bind(request); err != nil {	
+		c.String(http.StatusBadRequest, fmt.Sprintf("Cannot parse language data: %v", err.Error()))
+		return err
+	}
+
+	var err error
+	request.UserId, err = strconv.ParseUint(c.ParamOr("user_id", "0"), 10, 64)
+	if err != nil {
+		return err
+	}
+	
+	if c.Get("user_id") != request.UserId {
+		return c.String(http.StatusUnauthorized, "token user_id and put user_id not match")
+	}
+
+	return dbc.AddHobby(request)
+} 
+
+func deleteHobby(c *echo.Context) error {
+	userId, err := strconv.ParseUint(c.ParamOr("user_id", "0"), 10, 64)
+	if err != nil {
+		return err
+	}
+	
+	if c.Get("user_id") != userId {
+		return c.String(http.StatusUnauthorized, "token user_id and put user_id not match")
+	}
+	
+	hobbyId, err := strconv.ParseUint(c.ParamOr("hobby_id", "0"), 10, 64)
+	if err != nil {
+		return err
+	}
+
+	return dbc.DeleteHobby(userId, hobbyId)
+}

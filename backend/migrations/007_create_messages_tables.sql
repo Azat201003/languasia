49df7f37da86c2e64730a0bac3650d11d29573c1
zanelane <101720508+Azat201003@users.noreply.github.com>
2026-02-03 20:35:39 +0300

    Update 007_create_messages_tables.sql

--- backend/migrations/007_create_messages_tables.sql
+++ backend/migrations/007_create_messages_tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS messages (
 	message_id SERIAL PRIMARY KEY,
 	content VARCHAR(64),
-	sender_id SERIAL REFERENCES users(user_id)
+	sender_id SERIAL REFERENCES users(user_id),
 	chat_id SERIAL REFERENCES chats(chat_id)
 );
 

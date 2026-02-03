550e635e9d6cca9bdd78a9b5ef5bbc8ff3c1d70c
Azat201003 <zanelane87@gmail.com>
2026-02-03 20:27:35 +0300

    better search

--- backend/migrations/004_create_indexes.sql
+++ backend/migrations/004_create_indexes.sql
@@ -1,4 +1,3 @@
-CREATE INDEX idx_users_username ON users USING gin(username);
 CREATE INDEX idx_user_hobbies_user_id ON user_hobbies(user_id);
 CREATE INDEX idx_user_languages_user_id ON user_languages(user_id);
 CREATE INDEX idx_user_languages_is_known ON user_languages(is_known);

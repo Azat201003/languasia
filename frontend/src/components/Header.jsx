4ab19a0c2dbcdc290c29ff9d39dc3d50b3425c34
AbemeRules <awmbingo@gmail.com>
2026-03-05 13:38:24 +0300

    Add files via upload

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,25 +1,51 @@
-import React from 'react';
-import Logo from '../Logo.svg';
-import './Header.css';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useContext, useEffect } from 'react';
+import Logo from '../assets/Logo.svg';
+import "./Header.css";
+import api from "../api.jsx";
+import { redirect, useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const [color, setColor] = useState('#ffffff');
+  const [nickname, setNickname] = useState('');
+
   const navigate = useNavigate();
   
+  const api_url = import.meta.env.VITE_API_URL;
+
+  const user_id = localStorage.getItem('user_id');
+
+  useEffect(() => {
+    const fetchUserColor = async () => {
+        const userInfo = await fetch(api_url + `/users`, {method: "POST", headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ user_id: parseInt(user_id) }),});
+        const data = await userInfo.json();
+        setColor(data[0].color);
+        setNickname(data[0].nickname);
+    };
+
+    fetchUserColor();
+
+  }, [api_url, user_id]);
+  
   const onProfile = async (e) => {
-    navigate("/my")
+    navigate("/my");
   }
 
-  return (
-    <header className="top-header">
-      <a href="/">
-          <img src={Logo} alt="Logo" className="logo"/>
-      </a>
-      <a href="/messanger/">Messenger</a>
-      <a href="/search/">Find users</a>
-      <button className="profile-btn" aria-label="Профиль" onClick={onProfile} />
-    </header>
-  );
-}
+  const profileStyle = {backgroundColor: color}
 
-export default Header;
+  return (
+    <>
+      <header className="top-header">
+        <img src={Logo} alt="Logo" className="logo" />
+	<a href="/">Messenger</a>
+        <a href="/search">Search People</a>
+        <div className="profile-container">
+          <div className="nickname">{ nickname }</div>
+          <button style={profileStyle} className="profile-btn" aria-label="Профиль" onClick={onProfile} />
+        </div>
+      </header>
+    </>
+  );
+};
+
+export {Header};
+
